80d862bbf45f91288e45fe668f107bce4a67e801
Adil Moujahid <adil.mouja@gmail.com>
2013-07-07 15:35:24 +0000

    Modified web.js to fix error 3

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
     var buffer = 25;
-    response.send(fs.readFileSync("index.html"));
+    response.send(fs.readFileSync("index.html", "utf-8"));
 
 });
 
